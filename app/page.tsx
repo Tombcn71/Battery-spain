e0c709c1ff7aa8d9a385c0e912e044969131cc38
Tom <tbvanreijn@gmail.com>
2026-04-05 18:01:10 +0200

    camper

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     className="absolute inset-0 bg-cover bg-center slideshow-delay"
     style={{
       backgroundImage:
-        "url(https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80)",
+        "url(https://images.unsplash.com/photo-1670326457662-d981e6788945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80)",
     }}></div>
   {/* Overlay */}
   <div className="absolute inset-0 bg-black/50"></div>
