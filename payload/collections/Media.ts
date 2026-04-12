be4b3ad88742665cac225d5d46cdc753a0216c21
Tom <tbvanreijn@gmail.com>
2026-04-12 14:48:46 +0200

    Fix media local storage

--- payload/collections/Media.ts
+++ payload/collections/Media.ts
@@ -2,7 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 const Media: CollectionConfig = {
   slug: 'media',
-  upload: true,
+  upload: {
+    disableLocalStorage: true,
+  },
   fields: [
     { name: 'alt', type: 'text', label: 'Texto Alternativo' },
   ],
